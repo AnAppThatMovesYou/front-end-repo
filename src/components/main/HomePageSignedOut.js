f103f5997a56f2685cb5ba3afb1a0d3e314ba379
sanand100 <shraddhaanand1@gmail.com>
2022-07-26 12:01:37 -0400

    Create boilerplate code for components. Create and style navbarSignedOut

---
+++ src/components/main/HomePageSignedOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePageSignedOut(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default HomePageSignedOut;
---
+++ src/components/main/SignInForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignInForm(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SignInForm;
