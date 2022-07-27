332aabd3f25a1ffa09d2e7fb61d617dc31814dfa
JuliansRecent <juliansrecent@gmail.com>
2022-07-26 19:53:40 -0700

    Create styling for NavSignedOut. Add more font classes in index.css.

--- src/components/header/NavSignedOut.js
+++ src/components/header/NavSignedOut.js
@@ -23,7 +23,7 @@ function NavSignedOut(props) {
 						<span className='navbar-toggler-icon'></span>
 					</button>
 					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-						<ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+						<ul className='navbar-nav ms-auto mb-2 mb-lg-0 default-font light-text-color'>
 							<li className='nav-item'>
 								<a className='nav-link' href='#'>
 									Sign up
