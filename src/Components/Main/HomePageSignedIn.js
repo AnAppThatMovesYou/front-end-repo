4f47c9d22ed6f36b128c41764d62030214e44637
JuliansRecent <juliansrecent@gmail.com>
2022-08-02 10:05:18 -0700

    Fix blog-cards layout and styling.

--- src/Components/Main/HomePageSignedIn.js
+++ src/Components/Main/HomePageSignedIn.js
@@ -7,8 +7,6 @@ import './HomePageSignedIn.css';
 function HomePageSignedIn({ LogInJwt, currentBlogs, setCurrentBlogs }) {
 	let navigate = useNavigate();
 
-	
-
 	//fetch all the blogs
 	const getBlogs = async () => {
 		try {
@@ -29,136 +27,31 @@ function HomePageSignedIn({ LogInJwt, currentBlogs, setCurrentBlogs }) {
 		getBlogs();
 	}, []);
 
-	
 	return (
 		<div className='main-container container-fluid d-flex flex-column align-items-center'>
 			<p className='default-font home-title-in text-center'>
 				The blog for all of your moving needs
 			</p>
 
-			{currentBlogs.map((blogs) => {
-				return (
-					<Link to= {`/details/${blogs.id}`}>
-						<BlogContentCard
-							id={blogs.id}
-							title={blogs.title}
-							summary={blogs.summary}
-							content={blogs.content}
-							category={blogs.category}
-							imageUrl={blogs.imageUrl}
-							getBlogs={getBlogs}
-							// votes={post.votes}
-							key={blogs.id}
-						/>
-					</Link>
-					// <li className='blog-card' onClick= {(event)=> {
-					// 	event.preventDefault();
-					// 	navigate(`/details/${blogs.id}`)
-					// }}>
-					// 	<div className='img-container'>
-					// 		<img src={blogs.imageUrl} alt='' />
-					// 	</div>
-					// 	<div>
-					// 		<span className='blog-category'>{blogs.category}</span>
-					// 		<h1 className='blog-title'>{blogs.title}</h1>
-					// 		<div className='blog-description'>{blogs.summary}</div>
-					// 	</div>
-					// </li>
-				);
-			})}
-
-			{/* <ul className='blog-container'>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-				<li className='blog-card'>
-					<div className='img-container'>
-						<img
-							src='https://images.unsplash.com/photo-1658843941585-dd447ae263c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'
-							alt=''
-						/>
-					</div>
-					<div>
-						<span className='blog-category-in'>Category</span>
-						<div className='blog-title-in'>Blog title</div>
-						<div className='blog-description-in'>
-							Blog description, dolor sit amet consectetur adipisicing elit.
-						</div>
-					</div>
-				</li>
-			</ul> */}
+			<ul className='blog-container'>
+				{currentBlogs.map((blogs) => {
+					return (
+						<Link to={`/details/${blogs.id}`} className='blog-card shadow'>
+							<BlogContentCard
+								id={blogs.id}
+								title={blogs.title}
+								summary={blogs.summary}
+								content={blogs.content}
+								category={blogs.category}
+								imageUrl={blogs.imageUrl}
+								getBlogs={getBlogs}
+								// votes={post.votes}
+								key={blogs.id}
+							/>
+						</Link>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
